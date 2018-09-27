2c56c79cbcc4fdcac35a2e00a026778246f38845
Frooodooo <40638083+Frooodooo@users.noreply.github.com>
2018-09-27 23:25:14 +0300

    Initial commit

---
+++ Assets/GameManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+		public int selectedZombiePosition;
+		public GameObject selectedZombie;
+		public List<GameObject> zombies;
+		public Vector3 selectedSize;
+		public Vector3 defaultSize;
+
+		void Start(){
+			SelectZombie(zombies[0], 0); 
+
+		}
+		void Update(){
+			if (Input.GetKeyDown("left")){
+				ShiftLeft();
+			}
+			if (Input.GetKeyDown("right")){
+				ShiftRight();
+			}
+		}
+
+	void ShiftLeft(){
+		if(selectedZombiePosition == 0){
+			SelectZombie(zombies[3], 3);
+		}else{
+			SelectZombie(zombies[selectedZombiePosition - 1], selectedZombiePosition - 1); 
+		}
+	}
+	void ShiftRight(){
+		if(selectedZombiePosition == 3){
+			SelectZombie(zombies[0], 0);
+		}else{
+			SelectZombie(zombies[selectedZombiePosition + 1], selectedZombiePosition + 1); 
+		}
+	}
+		void SelectZombie(GameObject newZombie, int newPosition){
+			selectedZombie.transform.localScale = defaultSize;
+			selectedZombie = newZombie;
+			selectedZombie.transform.localScale = selectedSize;
+			selectedZombiePosition = newPosition;
+		}
+}
